b6ed725a9093f4d4596b584d71bad6d3e0931adc
sh-navid <shamshirsaz.navid@live.com>
2022-11-06 01:33:16 -0400

    Update server

--- frameworks/nodejs/default/create-server.js
+++ frameworks/nodejs/default/create-server.js
@@ -3,10 +3,14 @@
 http = require("http")
 
 const server = http.createServer((req, res) => {
-    if(req.url=="/"){
-
-    }else if (req.url=="/test"){
-
+    if (req.url == "/") {
+        r.writeHead(200, { 'Content-Type': 'text/html' });
+        r.write('<h1>Home Page</h1>');
+        r.end();
+    } else if (req.url == "/tasks") {
+        r.writeHead(200, { 'Content-Type': 'application/json' });
+        r.write({ "data": "This data passed from server to client" });
+        r.end();
     }
 })
 
