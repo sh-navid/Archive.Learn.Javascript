f277da1dae77bb1093c2e35c0b37dd9cef05ad28
sh-navid <shamshirsaz.navid@live.com>
2022-11-06 01:33:56 -0400

    Fixed naming

--- frameworks/nodejs/default/create-server.js
+++ frameworks/nodejs/default/create-server.js
@@ -4,13 +4,13 @@ http = require("http")
 
 const server = http.createServer((req, res) => {
     if (req.url == "/") {
-        r.writeHead(200, { 'Content-Type': 'text/html' });
-        r.write('<h1>Home Page</h1>');
-        r.end();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>Home Page</h1>');
+        res.end();
     } else if (req.url == "/tasks") {
-        r.writeHead(200, { 'Content-Type': 'application/json' });
-        r.write({ "data": "This data passed from server to client" });
-        r.end();
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write({ "data": "This data passed from server to client" });
+        res.end();
     }
 })
 
