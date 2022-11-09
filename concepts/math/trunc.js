84d1ddd48a7a8e70b0eec666966b582cbcc58b5d
sh-navid <shamshirsaz.navid@live.com>
2022-11-10 00:06:09 +0330

    Update

---
+++ concepts/math/trunc.js
@@ -0,0 +1,13 @@
+console.log(Math.trunc(3.3))
+console.log(Math.trunc(-3.3))
+
+
+console.log(Math.floor(3.3))
+console.log(Math.floor(-3.3))
+
+
+console.log(Math.ceil(3.3))
+
+
+console.log(Math.round(3.6))
+console.log(Math.round(3.3))
