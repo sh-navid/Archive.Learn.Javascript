bfbca91b776ef3f8de46ce085b4a98354952f82a
sh-navid <shamshirsaz.navid@live.com>
2022-11-24 05:11:27 +0330

    Update

--- frameworks/reactjs/example-api/src/components/Components.jsx
+++ frameworks/reactjs/example-api/src/components/Components.jsx
@@ -1,25 +1,42 @@
 import { useState } from "react"
-import "./Components.sass"
+import "./Components.css"
 
 const data = [
     {
         "question": "What is the capital of Iran?",
         "answers": ["Tehran", "Paris", "London"],
-        "correct": 0
+        "correct": 0,
+        "selected": -1
     }, {
         "question": "What is the capital of England?",
         "answers": ["Manhatan", "Tokio", "London"],
-        "correct": 2
+        "correct": 2,
+        "selected": -1
     }
 ]
 
 
 const Answers = (props) => {
+    const getClassName = (index) => {
+        console.log(index, props.data.selected)
+        if (index == props.data.selected) {
+            console.log("ACTIVE")
+            return "answer active"
+        }
+        else {
+            console.log("NOT ACTIVE")
+            return "answer"
+        }
+    }
     return (
         <>
-            {props.data.map((item, index) => (
-                <span className="answer"
-                    onClick={() => props.change(index)}>{item}</span>
+            {props.data.answers.map((item, index) => (
+                <span
+                    key={index}
+                    className={getClassName(index)}
+                    onClick={() => props.change(index)}>
+                    {item}
+                </span>
             ))}
         </>
     )
@@ -29,8 +46,12 @@ const Controls = (props) => {
     return (
         <>
             {props.data.map((item, index) => (
-                <span className="control"
-                    onClick={() => props.change(index)}>{index + 1}</span>
+                <span
+                    key={index}
+                    className="control"
+                    onClick={() => props.change(index)}>
+                    {index + 1}
+                </span>
             ))}
         </>
     )
@@ -41,12 +62,25 @@ const Controls = (props) => {
 // Why we use state instead of a variable?
 const Questions = () => {
     const [question, setQuestion] = useState(0)
+    const [update, setUpdate] = useState(false)
+
+    const calculateScore=()=>{
+        let score=0
+        for (let d of data){
+            if(d.correct==d.selected){
+                score+=1
+            }
+        }
+        return score
+    }
 
     return (
         <div>
             <span>{data[question].question}</span>
             <hr />
-            <Answers data={data[question].answers} change={() => { }} />
+            <span>Score: {calculateScore()}</span>
+            <hr />
+            <Answers data={data[question]} change={(i) => { data[question].selected = i; setUpdate(!update) }} />
             <hr />
             <Controls data={data} change={(i) => { setQuestion(i) }} />
         </div>
