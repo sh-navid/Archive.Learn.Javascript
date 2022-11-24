cc8435b822c406046eb8a5cb3ac7ed2ef3b0293f
sh-navid <shamshirsaz.navid@live.com>
2022-11-24 04:40:01 +0330

    Update

--- frameworks/reactjs/example-api/src/App.js
+++ frameworks/reactjs/example-api/src/App.js
@@ -1,10 +1,13 @@
+import Questions from "./components/Components";
 
 function App() {
   return (
     <>
-     
+    AAA
+    <hr/>
+    <Questions/>
     </>
-  );
+  )
 }
 
 export default App;
--- frameworks/reactjs/example-api/src/components/Components.jsx
+++ frameworks/reactjs/example-api/src/components/Components.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import "./Components.sass"
 
-const list = [
+const data = [
     {
         "question": "What is the capital of Iran?",
         "answers": ["Tehran", "Paris", "London"],
@@ -13,14 +13,42 @@ const list = [
     }
 ]
 
+
+const Answers = (props) => {
+    return (
+        <>
+            {props.data.map((item, index) => (
+                <span className="answer"
+                    onClick={() => props.change(index)}>{item}</span>
+            ))}
+        </>
+    )
+}
+
+const Controls = (props) => {
+    return (
+        <>
+            {props.data.map((item, index) => (
+                <span className="control"
+                    onClick={() => props.change(index)}>{index + 1}</span>
+            ))}
+        </>
+    )
+}
+
+
+// What is a hook?
 // Why we use state instead of a variable?
 const Questions = () => {
     const [question, setQuestion] = useState(0)
+
     return (
         <div>
-            <span>{list[question]}</span>
-            <hr/>
-            <span></span>
+            <span>{data[question].question}</span>
+            <hr />
+            <Answers data={data[question].answers} change={() => { }} />
+            <hr />
+            <Controls data={data} change={(i) => { setQuestion(i) }} />
         </div>
     )
 }
