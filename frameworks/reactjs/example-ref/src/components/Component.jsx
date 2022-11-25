6405c016733573e8169089e080e1c03848b03ffb
sh-navid <shamshirsaz.navid@live.com>
2022-11-25 07:27:48 +0330

    Update

--- frameworks/reactjs/example-ref/src/components/Component.jsx
+++ frameworks/reactjs/example-ref/src/components/Component.jsx
@@ -1,7 +1,21 @@
+import React from "react"
+
+const MyButton = React.forwardRef((props, ref) => {
+    return (
+        <button onClick={props.handle} ref={ref}>Hello</button>
+    )
+})
+
 const Component = () => {
+    const ref = React.createRef()
+
+    const handleClick=()=>{
+        ref.current.innerHTML="HI HI"
+    }
+
     return (
         <>
-            COM
+            <MyButton ref={ref} handle={handleClick}/>
         </>
     )
 }
