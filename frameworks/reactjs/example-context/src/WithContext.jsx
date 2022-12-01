ae97db591d2dcd4749c463a387ec6ff637dab2a2
sh-navid <shamshirsaz.navid@live.com>
2022-12-01 07:59:57 +0330

    Update

--- frameworks/reactjs/example-context/src/WithContext.jsx
+++ frameworks/reactjs/example-context/src/WithContext.jsx
@@ -1,32 +1,39 @@
-import { useState } from "react"
+import { createContext } from "react"
+import { useContext, useState } from "react"
+
+const DataContext = createContext()
 
 const A = () => {
     const [data, setData] = useState(10)
 
     return (
-        <>
+        <DataContext.Provider value={data}>
             <h1>A</h1>
             {data}
             <button onClick={() => { setData(data + 1) }}>UP</button>
             <hr />
-            <B data={data} handle={() => { setData(data + 2) }} />
-        </>
+            <B />
+        </DataContext.Provider>
     )
 }
 
-const B = (props) => {
+const B = () => {
+    const data = useContext(DataContext)
+
     return (
         <>
             <h2>B</h2>
-            {props.data}
-            <button onClick={props.handle}>UP2</button>
+            {data}
+            <button onClick={setData(20)}>UP2</button>
             <hr />
-            <C data={props.data} />
+            <C />
         </>
     )
 }
 
-const C = ({ data }) => {
+const C = () => {
+    const data = useContext(DataContext)
+
     return (
         <>
             <h3>C</h3>
